b9407e27f16bd213a2b6a2dedd600fc4d9398ee4
Rachel Moskowitz <rachieb19@gmail.com>
2021-10-29 14:52:30 +0000

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const router = require("./routes/taskRoutes.js")
 const cors = require("cors");
 // boilerplate - allow us to access req.body for post and update
