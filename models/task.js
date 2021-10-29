92719170512a2597c82de32fa5cf5856d8d921ad
Rachel Moskowitz <rachieb19@gmail.com>
2021-10-29 15:16:12 +0000

    added rejection clause

--- models/task.js
+++ models/task.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize")
 const url = process.env.DATABASE_URL ||"postgres://gzznihge:zlu7NCfEmC8I2QbS5ICTTSXnbe0HDAPK@fanny.db.elephantsql.com/gzznihge"
 const sequelize = new Sequelize(url,{"dialect": "postgres",
 "dialectOptions": {
-  "ssl": true
+  "ssl": true,
+    "rejectUnauthorized": false
 }})
 
 let Task = sequelize.define("tasks", {
