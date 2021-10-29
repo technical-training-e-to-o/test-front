511907c4d95e25d349164e019bc730c28cfaf4c7
Rachel Moskowitz <rachieb19@gmail.com>
2021-10-29 15:52:26 +0000

    models

--- models/task.js
+++ models/task.js
@@ -1,10 +1,16 @@
 const Sequelize = require("sequelize")
 const url = process.env.DATABASE_URL ||"postgres://gzznihge:zlu7NCfEmC8I2QbS5ICTTSXnbe0HDAPK@fanny.db.elephantsql.com/gzznihge"
-const sequelize = new Sequelize(url,{"dialect": "postgres",
-"dialectOptions": {
-  "ssl": true,
-    "rejectUnauthorized": false
-}})
+const sequelize = new Sequelize(url,{    dialect: 'postgres',
+    ssl: true,
+    protocol: "postgres",
+
+    logging: true,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // <<<<<< YOU NEED THIS
+        }
+    })
 
 let Task = sequelize.define("tasks", {
   name: Sequelize.STRING,
